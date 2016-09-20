66cd7c840c1980cab7447a59bc4ac683889aafe9
Zack Chua <zzchua@cs.washington.edu>
2016-09-20 23:36:07 +0800

    deploying to heroku...

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ Angular2 Application using Voicebox ASR and TTS technologies.
 
 These APIs can be found on developer.voicebox.com
 
-To run the app: 
-* Run `npm install` inside the project folder to download all the dependencies. This only needs to be done once.
-* Run `npm run build` to compile the TypeScript code in the `src` folder into ES5 code into the `app` folder.
+To run the server and application locally:
+* Run `npm install` inside the project folder to download and install the required dependencies.
+* Run `npm run build` to compile and bundle the application using webpack.
 * Run `npm run serve` to start a local development web server. You can now access the application at [localhost:8080](http://localhost:8080/).
--- src/server/server.js
+++ src/server/server.js
@@ -1,35 +1,15 @@
 var express = require('express');
 var path = require('path');
-var cors = require('cors');
 
-
-// var fs = require('fs');
-// var https = require('https');
-
-// var privateKey = fs.readFileSync('public/key.pem');
-// var privateCert = fs.readFileSync('public/key-cert.pem');
 var app = express();
 
 
-
-// var credentials = { key: privateKey, cert: privateCert};
-
-// console.log(path.join(__dirname, '../../keycloak.js'));
-
-app.use(cors());
+// app.use(cors());
 app.use(express.static(path.join(__dirname, '../../public')));
 app.use(express.static(path.join(__dirname, '../../dist')));
 
 
 
-// var server = https.createServer(credentials, app)
-
-app.listen(8080, function() {
+app.listen(app.env.PORT || 8080, function() {
     console.log('server started');
 });
-
-
-// nginix on AWS serves on port 8081
-// server.listen(8081, "localhost", function() {
-//     console.log("https server started");
-// })
